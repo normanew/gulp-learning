d765fb047e97fb09b0839de6a82951140c12b846
Norman Wijaya <norman.emry@gmail.com>
2015-04-23 11:31:22 +0700

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var coffee = require('gulp-coffee');
+var es = require('event-stream');
+
+gulp.task('scripts', function() {
+  var javaScriptFromCoffeeScript = gulp.src('src/*.coffee')
+    .pipe(coffee());
+
+  var js = gulp.src('src/*.js');
+
+  return es.merge(javaScriptFromCoffeeScript, js)
+  .pipe(concat('all.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/*.{js,coffee}', ['scripts']);
+});
